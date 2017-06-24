274651b754178abdb5fa594057dc910376fa60fd
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2017-06-25 02:07:45 +0300

    changes

--- src/main/java/com/acme/cloud/controller/CloudController.java
+++ src/main/java/com/acme/cloud/controller/CloudController.java
@@ -1,6 +1,6 @@
 package com.acme.cloud.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
